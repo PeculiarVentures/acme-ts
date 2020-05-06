9f57956fdb4c0912ee65677d08077f7e61ec49bc
Roman Miroshin <romashine@Romans-MacBook-Pro.local>
2020-05-06 17:03:20 +0300

    feat(core): added enums

--- packages/core/src/crypto/index.ts
+++ packages/core/src/crypto/index.ts
@@ -2,5 +2,7 @@ export * from "./cert";
 export * from "./cert_req";
 export * from "./signature_algorithm";
 export * from "./validity_period";
+export * from "./oid_extensions";
+export * from "./oid_attributes";
 
 export * from "./attributes";
---
+++ packages/core/src/crypto/oid_extensions.ts
@@ -0,0 +1,9 @@
+export enum OidExtensions
+    {
+        subjectKeyIdentifier = "2.5.29.14",
+        extKeyUsage = "2.5.29.37",
+        keyUsage = "2.5.29.15",
+        basicConstraints = "2.5.29.19",
+        templateV1 = "1.3.6.1.4.1.311.20.2",
+        templateV2 = "1.3.6.1.4.1.311.21.7",
+    }
