73c1746de059ad1f8bfd0c505c65a292ecebd4c8
microshine <microshine@mail.ru>
2020-05-06 17:50:08 +0300

    test(jose): RSASSA and HMAC tests

--- test/core/jose.ts
+++ test/core/jose.ts
@@ -1,3 +1,4 @@
+import * as assert from "assert";
 import { JsonWebSignature } from "@peculiar/acme-core";
 import { Crypto } from "@peculiar/webcrypto";
 
@@ -10,6 +11,7 @@ context("jose", () => {
     context("sign/verify", () => {
 
       it("RSASSA-PKCS1-v1_5 SHA256", async () => {
+        // generate keys
         const alg = {
           name: "RSASSA-PKCS1-v1_5",
           hash: "SHA-256",
@@ -19,7 +21,6 @@ context("jose", () => {
         const keys = await crypto.subtle.generateKey(alg, false, ["sign", "verify"]);
 
         const jws = new JsonWebSignature(crypto);
-
         jws.setProtected({
           jwk: await crypto.subtle.exportKey("jwk", keys.publicKey),
           nonce: "nonce_value",
@@ -29,7 +30,34 @@ context("jose", () => {
 
         await jws.sing(alg, keys.privateKey);
 
-        console.log(`${jws.protected}.${jws.payload}.${jws.signature}`);
+        jws.parse(jws.toString());
+
+        const ok = await jws.verify(keys.publicKey);
+        assert.equal(ok, true);
+      });
+
+      it("HMAC SHA512", async () => {
+        // generate keys
+        const alg = {
+          name: "HMAC",
+          hash: "SHA-512",
+          length: 256,
+        } as HmacKeyGenParams;
+        const key = await crypto.subtle.generateKey(alg, false, ["sign", "verify"]);
+
+        const jws = new JsonWebSignature(crypto);
+        jws.setProtected({
+          kid: "http://key.id",
+          nonce: "nonce_value",
+          url: "http://test.url",
+        })
+
+        await jws.sing(alg, key);
+
+        jws.parse(jws.toString());
+
+        const ok = await jws.verify(key);
+        assert.equal(ok, true);
       });
 
     });
