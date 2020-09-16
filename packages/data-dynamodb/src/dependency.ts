489d4d840410b708620006373db1b315be601dbe
romashine <miroshin.ry@gmail.com>
2020-09-16 15:13:21 +0300

    chore(data-dynamodb): Options dynomodb

--- packages/data-dynamodb/src/dependency.ts
+++ packages/data-dynamodb/src/dependency.ts
@@ -2,10 +2,16 @@ import { DependencyContainer, Lifecycle } from "tsyringe";
 import * as data from "@peculiar/acme-data";
 import * as repositories from "./repositories";
 import * as models from "./models";
-
+import * as dynamoose from "dynamoose";
+import { DynamoDB } from "aws-sdk";
 
 export class DependencyInjection {
-  public static register(container: DependencyContainer) {
+  public static register(container: DependencyContainer, options: DynamoDB.ClientConfiguration) {
+    const ddb = new dynamoose.aws.sdk.DynamoDB(options);
+
+    // Set DynamoDB instance to the Dynamoose DDB instance
+    dynamoose.aws.ddb.set(ddb);
+
     data.DependencyInjection.register(container);
     container.register(data.diAccount, models.Account)
       .register(data.diAuthorization, models.Authorization)
@@ -14,14 +20,14 @@ export class DependencyInjection {
       .register(data.diError, models.DynamoError)
       .register(data.diExternalAccount, models.ExternalAccount)
       .register(data.diOrderAuthorization, models.OrderAuthorization)
-      .register(data.diOrder, models.Order)
+      .register(data.diOrder, models.Order);
 
-    container.register(data.diAccountRepository, repositories.AccountRepository, {lifecycle: Lifecycle.Singleton});
-    container.register(data.diAuthorizationRepository, repositories.AuthorizationRepository, {lifecycle: Lifecycle.Singleton});
-    container.register(data.diChallengeRepository, repositories.ChallengeRepository, {lifecycle: Lifecycle.Singleton});
-    container.register(data.diExternalAccountRepository, repositories.ExternalAccountRepository, {lifecycle: Lifecycle.Singleton});
-    container.register(data.diNonceRepository, repositories.NonceRepository, {lifecycle: Lifecycle.Singleton});
-    container.register(data.diOrderRepository, repositories.OrderRepository, {lifecycle: Lifecycle.Singleton});
-    container.register(data.diOrderAuthorizationRepository, repositories.OrderAuthorizationRepository, {lifecycle: Lifecycle.Singleton});
+    container.register(data.diAccountRepository, repositories.AccountRepository, { lifecycle: Lifecycle.Singleton });
+    container.register(data.diAuthorizationRepository, repositories.AuthorizationRepository, { lifecycle: Lifecycle.Singleton });
+    container.register(data.diChallengeRepository, repositories.ChallengeRepository, { lifecycle: Lifecycle.Singleton });
+    container.register(data.diExternalAccountRepository, repositories.ExternalAccountRepository, { lifecycle: Lifecycle.Singleton });
+    container.register(data.diNonceRepository, repositories.NonceRepository, { lifecycle: Lifecycle.Singleton });
+    container.register(data.diOrderRepository, repositories.OrderRepository, { lifecycle: Lifecycle.Singleton });
+    container.register(data.diOrderAuthorizationRepository, repositories.OrderAuthorizationRepository, { lifecycle: Lifecycle.Singleton });
   }
 }
--- packages/data-dynamodb/src/index.ts
+++ packages/data-dynamodb/src/index.ts
@@ -1,13 +1,2 @@
 export * from "./repositories";
 export * from "./dependency";
-
-import * as dynamoose from "dynamoose";
-const ddb = new dynamoose.aws.sdk.DynamoDB({
-  "accessKeyId": "12345678",
-  "secretAccessKey": "12345678",
-  "region": "local",
-  "endpoint": "http://localhost:8000",
-});
-
-// Set DynamoDB instance to the Dynamoose DDB instance
-dynamoose.aws.ddb.set(ddb);
