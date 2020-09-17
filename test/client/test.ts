dc020e21d1c17cea6df98c2b2244779652e88450
microshine <microshine@mail.ru>
2020-09-17 22:01:13 +0300

    test(client): Add Lets Encrypt content

--- test/client/test.ts
+++ test/client/test.ts
@@ -8,24 +8,28 @@ context("client", () => {
   const crypto = new Crypto();
   cryptoProvider.set(crypto);
 
-  it("create user", async () => {
-    const alg: RsaHashedKeyGenParams = {
-      name: "RSASSA-PKCS1-v1_5",
-      hash: "SHA-256",
-      publicExponent: new Uint8Array([1, 0, 1]),
-      modulusLength: 2048,
-    };
-    const keys = (await crypto.subtle.generateKey(alg, false, ["sign", "verify"])) as CryptoKeyPair;
+  context("Lets Encrypt", () => {
 
-    const client = new ApiClient(keys, "https://acme-staging-v02.api.letsencrypt.org/directory", {
-      fetch: fetch as any,
-      debug: false,
+    it("create user", async () => {
+      const alg: RsaHashedKeyGenParams = {
+        name: "RSASSA-PKCS1-v1_5",
+        hash: "SHA-256",
+        publicExponent: new Uint8Array([1, 0, 1]),
+        modulusLength: 2048,
+      };
+      const keys = (await crypto.subtle.generateKey(alg, false, ["sign", "verify"])) as CryptoKeyPair;
+
+      const client = new ApiClient(keys, "https://acme-staging-v02.api.letsencrypt.org/directory", {
+        fetch: fetch as any,
+        debug: false,
+      });
+      await client.initialize();
+      await client.newAccount({
+        contact: ["mailto:microshine@mail.ru"],
+        termsOfServiceAgreed: true,
+      });
     });
-    await client.initialize();
-    await client.newAccount({
-      contact: ["mailto:microshine@mail.ru"],
-      termsOfServiceAgreed: true,
-    });
+
   });
 
 }).timeout(5e3);
