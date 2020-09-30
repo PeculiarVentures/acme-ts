c75ec65a1e9785053e1f33139541ab105acadc54
microshine <microshine@mail.ru>
2020-10-01 02:17:03 +0300

    chore(client): Update rollup configuration

--- packages/client/rollup.config.js
+++ packages/client/rollup.config.js
@@ -1,83 +1,62 @@
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
-import typescript from "@rollup/plugin-typescript";
-import babel from "rollup-plugin-babel";
+import typescript from "rollup-plugin-typescript2";
+import { getBabelOutputPlugin } from "@rollup/plugin-babel";
 import { terser } from "rollup-plugin-terser";
 
+// eslint-disable-next-line @typescript-eslint/no-var-requires, no-undef
 const pkg = require("./package.json");
 
 const banner = [].join("\n");
 const input = "src/index.ts";
 
-const browserExternals = {
-};
 const browser = [
   {
     input,
     plugins: [
       resolve({
-        mainFields: ["jsnext:main", "module", "main"],
+        mainFields: ["esnext", "module", "main"],
         preferBuiltins: true,
       }),
       commonjs(),
       typescript({
-        module: "ES2015",
-        removeComments: true,
+        check: true,
+        clean: true,
+        tsconfigOverride: {
+          compilerOptions: {
+            module: "es2015",
+          }
+        }
       }),
     ],
-    external: Object.keys(browserExternals),
     output: [
       {
+        banner,
         file: pkg.browser,
-        format: "es",
-        globals: browserExternals,
+        format: "iife",
+        plugins: [
+          getBabelOutputPlugin({
+            allowAllFormats: true,
+            presets: [
+              ["@babel/preset-env", {
+                targets: {
+                  chrome: "60"
+                },
+              }],
+            ],
+          }),
+          terser({
+            compress: {
+              keep_classnames: true,
+            },
+            format: {
+              comments: false,
+            },
+          }),
+        ],
+        name: "acme"
       }
     ]
-  },
-  {
-    input: pkg.browser,
-    external: Object.keys(browserExternals),
-    plugins: [
-      babel({
-        babelrc: false,
-        runtimeHelpers: true,
-        compact: false,
-        comments: false,
-        presets: [
-          ["@babel/env", {
-            targets: {
-              // ie: "11",
-              chrome: "60",
-            },
-            useBuiltIns: "entry",
-            corejs: 3,
-          }],
-        ],
-        plugins: [
-          ["@babel/plugin-proposal-class-properties"],
-          ["@babel/proposal-object-rest-spread"],
-        ]
-      }),
-    ],
-    output: [
-      {
-        banner,
-        file: pkg.browser,
-        globals: browserExternals,
-        format: "iife",
-        name: "acme",
-      },
-      {
-        banner,
-        file: pkg.browserMin,
-        globals: browserExternals,
-        format: "iife",
-        name: "acme",
-        plugins: [
-          terser(),
-        ]
-      },
-    ],
   },
 ];
 
