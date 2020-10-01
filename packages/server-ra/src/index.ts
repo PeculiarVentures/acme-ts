2ca8bf2cc40df86bec423ebbfc294a03550ea83e
romashine <miroshin.ry@gmail.com>
2020-10-01 12:46:38 +0300

    feat(server-ra): Remove data-memory dependency

--- packages/server-ra/src/index.ts
+++ packages/server-ra/src/index.ts
@@ -2,7 +2,6 @@ import { Request, Response, Express } from 'express';
 import { diLogger, ConsoleLogger } from "@peculiar/acme-core";
 import { cryptoProvider } from "@peculiar/x509";
 import { AcmeExpress, diControllers } from "@peculiar/acme-express";
-import { DependencyInjection as diData } from "@peculiar/acme-data-memory";
 import * as acmeServer from "@peculiar/acme-server";
 import { Crypto } from "@peculiar/webcrypto";
 import { container, Lifecycle } from "tsyringe";
@@ -37,11 +36,9 @@ export class AcmeRa {
     container.register(acmeServer.diCertificateEnrollmentService, services.CertificateEnrollmentService, { lifecycle: Lifecycle.Singleton });
     container.register(acmeServer.diIdentifierService, services.EmailChallengeService);
     container.register(acmeServer.diDirectoryService, services.RaDirectoryService);
-    container.register(services.diAuth0, services.Auth0Service);
     container.register(diControllers, RaControllers);
     container.register(acmeServer.diExternalAccountService, acmeServer.ExternalAccountService);
     container.register(acmeServer.diAuthorizationService, services.RaAuthorizationService);
     container.register(services.diAuth0, services.Auth0Service);
-    diData.register(container);
   }
 }
