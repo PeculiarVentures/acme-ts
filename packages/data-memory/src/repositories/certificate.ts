e944a4ef2b3591a13a72f087be55148bed48bbe2
romashine <miroshin.ry@gmail.com>
2020-10-09 09:25:50 +0300

    feat(data-memory): Added type to Certificate

--- packages/data-memory/src/repositories/certificate.ts
+++ packages/data-memory/src/repositories/certificate.ts
@@ -2,6 +2,10 @@ import { ICertificate, ICertificateRepository } from "@peculiar/acme-data";
 import { BaseRepository } from "./base";
 
 export class CertificateRepository extends BaseRepository<ICertificate> implements ICertificateRepository {
+  public async findCaCertificates(): Promise<ICertificate[] | null> {
+    const caCerts = this.items.filter(o => o.type === "ca");
+    return caCerts ? caCerts : null;
+  }
 
   public async findByThumbprint(thumbprint: string) {
     const item = this.items.find(o => o.thumbprint === thumbprint);
