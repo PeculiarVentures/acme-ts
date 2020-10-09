a223c37fdbb1368be2d7cf4cd6e8350ca5430d5b
romashine <miroshin.ry@gmail.com>
2020-10-09 14:53:30 +0300

    fix(core): Switch case to onWrite

--- packages/core/src/logger/console_logger.ts
+++ packages/core/src/logger/console_logger.ts
@@ -3,26 +3,26 @@ import { Logger, LoggerData, LoggerLevel } from "./logger";
 export class ConsoleLogger extends Logger {
 
   protected onWrite(lvl: LoggerLevel, msg: string, obj?: LoggerData) {
-    switch (lvl.toString()) {
-      case "debug":
+    switch (lvl) {
+      case LoggerLevel.debug:
         if (obj) {
           console.debug(msg, obj);
         }
         console.debug(msg);
         break;
-      case "warning":
+      case LoggerLevel.warn:
         if (obj) {
           console.warn(msg, obj);
         }
         console.warn(msg);
         break;
-      case "error":
+      case LoggerLevel.error:
         if (obj) {
           console.error(msg, obj);
         }
         console.error(msg);
         break;
-      case "info":
+      case LoggerLevel.info:
       default:
         if (obj) {
           console.info(msg, obj);
