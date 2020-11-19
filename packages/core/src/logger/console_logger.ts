ea55e3a4a341b91cc29d738c6a3b9d740fbde3cc
microshine <microshine@mail.ru>
2020-11-19 16:17:32 +0300

    feat(core): Improve ConsoleLogger

--- packages/core/src/logger/console_logger.ts
+++ packages/core/src/logger/console_logger.ts
@@ -7,27 +7,31 @@ export class ConsoleLogger extends Logger {
       case LoggerLevel.debug:
         if (obj) {
           console.debug(msg, obj);
+        } else {
+          console.debug(msg);
         }
-        console.debug(msg);
         break;
       case LoggerLevel.warn:
         if (obj) {
           console.warn(msg, obj);
+        } else {
+          console.warn(msg);
         }
-        console.warn(msg);
         break;
       case LoggerLevel.error:
         if (obj) {
           console.error(msg, obj);
+        } else {
+          console.error(msg);
         }
-        console.error(msg);
         break;
       case LoggerLevel.info:
       default:
         if (obj) {
           console.info(msg, obj);
+        } else {
+          console.log(msg);
         }
-        console.log(msg);
         break;
     }
   }
