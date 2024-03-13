50caf1e82884e99d0daf6903c0e042804808596b
seriy379 <seriy379@gmail.com>
2024-03-13 10:52:28 +0200

    chore(data): Extend account, externalAccount interfaces

--- packages/data/src/models/account.ts
+++ packages/data/src/models/account.ts
@@ -14,6 +14,7 @@ export interface IAccount extends IBaseObject {
   thumbprint: string;
   termsOfServiceAgreed?: boolean;
   externalAccountId?: Key;
+  providerIdentifier?: string;
   contacts?: string[];
   createdAt: Date;
 }
